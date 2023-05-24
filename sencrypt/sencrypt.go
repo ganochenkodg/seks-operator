a41feda6872dfa523dc5d5a8b06dc2e4ab2447b9
ganochenkodg <52278182+ganochenkodg@users.noreply.github.com>
2023-05-24 11:38:22 +0200

    Update sencrypt.go

--- sencrypt/sencrypt.go
+++ sencrypt/sencrypt.go
@@ -47,7 +47,7 @@ func GetKey() *rsa.PublicKey {
 
 func GetData() map[string]interface{} {
 	file := os.Stdin
-  fi, _ := file.Stat()
+        fi, _ := file.Stat()
 	size := fi.Size()
 	if size == 0 {
 		fmt.Println("Stdin is empty, please pass some Secret")
